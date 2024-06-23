31244a21c7f83de681e583c1aaa79466a39958a1
TechShreyash <82265247+TechShreyash@users.noreply.github.com>
2024-06-23 19:46:56 +0530

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -4,26 +4,32 @@ import asyncio
 import inspect
 from tqdm import tqdm
 from pathlib import Path
-from extra import change_file_path_if_exist, get_random_string, AdjustableSemaphore,get_filename
+from extra import (
+    change_file_path_if_exist,
+    get_random_string,
+    AdjustableSemaphore,
+    get_filename,
+)
 from logger import Logger
-from typing import Callable, Any, Union, Awaitable
+from typing import Callable, Any, Union, Awaitable, Optional
 from curl_cffi.requests import AsyncSession
+import time
 
 
 class FileDownloader:
     def __init__(
         self,
         url: str,
-        custom_headers: dict = None,
+        custom_headers: Optional[dict] = None,
         output_dir: Union[str, Path] = Path("downloads"),
-        filename: str = None,
-        workers: int = None,
+        filename: Optional[str] = None,
+        workers: Optional[int] = None,
         initial_dynamic_workers: int = 2,
         dynamic_workers_update_interval: int = 5,
         debug: bool = True,
         progress: bool = True,
-        progress_callback: Union[
-            Callable[..., Any], Callable[..., Awaitable[Any]]
+        progress_callback: Optional[
+            Union[Callable[..., Any], Callable[..., Awaitable[Any]]]
         ] = None,
         progress_args: tuple = (),
         progress_interval: int = 1,
@@ -37,21 +43,21 @@ class FileDownloader:
 
         Args:
             url (str): URL of the file to download.
-            custom_headers (dict, optional): Custom headers to send with the request. Defaults to None.
+            custom_headers (Optional[dict], optional): Custom headers to send with the request. Defaults to None.
             output_dir (Union[str, Path], optional): Directory where the file will be saved. Defaults to "downloads".
-            filename (str, optional): Name to save the file as (including extension). By default this will determined automatically, from headers or url, if failed then a default id will be used as filename.
-            workers (int, optional): Number of fixed concurrent download workers. By default this will be dynamically changed based on the download speed.
-            initial_dynamic_workers (int, optional): Number of initial dynamic workers count to start with when dynamically determining workers. Defaults to 2.
-            dynamic_workers_update_interval (int,optional): Interval in seconds after which the dynamic worker count will get updated, Default 5 seconds
-            debug (bool, optional): True to get debug logs. Defaults to True.
-            progress (bool, optional): True if you want tqdm download progress. Defaults to True.
-            progress_callback (Union[Callable[..., Any], Callable[..., Awaitable[Any]]], optional):
-                Callback function to update download progress. Can be sync or async. Defaults to None. Setting this will disable tqdm progress.
+            filename (Optional[str], optional): Name to save the file as (including extension). By default, this will be determined automatically.
+            workers (Optional[int], optional): Number of fixed concurrent download workers. By default, this will be dynamically changed based on the download speed.
+            initial_dynamic_workers (int, optional): Initial number of dynamic workers. Defaults to 2.
+            dynamic_workers_update_interval (int, optional): Interval in seconds to update dynamic worker count. Defaults to 5.
+            debug (bool, optional): Enable debug logs. Defaults to True.
+            progress (bool, optional): Enable download progress display. Defaults to True.
+            progress_callback (Optional[Union[Callable[..., Any], Callable[..., Awaitable[Any]]]], optional):
+                Callback function for download progress updates. Can be sync or async. Defaults to None. Setting this disables tqdm progress.
             progress_args (tuple, optional): Additional arguments for progress_callback. Defaults to ().
             progress_interval (int, optional): Time interval for progress updates in seconds. Defaults to 1.
-            chunk_size (int, optional): Size of each download chunk in bytes. Defaults to 5 MB. Dont make it too small or too large (more than 10 mb not recommended).
-            single_threaded (bool, optional): True if you want to use single-threaded download. Defaults to False, will be determined automatically based on the download url server.
-            max_retries (int, optional): Number of retries for each chunk/file download. Defaults to 3.
+            chunk_size (int, optional): Size of each download chunk in bytes. Defaults to 5 MB.
+            single_threaded (bool, optional): Force single-threaded download. Defaults to False.
+            max_retries (int, optional): Maximum retries for each chunk/file download. Defaults to 3.
             timeout (int, optional): Timeout for each request in seconds. Defaults to 60.
         """
         self.id = get_random_string(6)
@@ -89,7 +95,13 @@ class FileDownloader:
         if self.debug:
             self.logger.debug(message)
 
-    async def task_runner(self, tasks):
+    async def task_runner(self, tasks: list[Awaitable]) -> None:
+        """
+        Run a list of async tasks concurrently, handling exceptions and cancellations.
+
+        Args:
+            tasks (list[Awaitable]): List of async tasks to run.
+        """
         new_tasks = [asyncio.create_task(task) for task in tasks]
         tasks = new_tasks
         done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_EXCEPTION)
@@ -98,7 +110,6 @@ class FileDownloader:
             if task.exception():
                 for pending_task in pending:
                     pending_task.cancel()
-                # Wait for the cancellation to complete
                 await asyncio.gather(*pending, return_exceptions=True)
                 raise task.exception()
 
@@ -151,7 +162,18 @@ class FileDownloader:
                         await asyncio.sleep(self.progress_interval)
                     pbar.update(self.total_size - previous_size)
 
-    async def load_chunk(self, temp_file_path, start, end, semaphore) -> None:
+    async def load_chunk(
+        self, temp_file_path: Path, start: int, end: int, semaphore: AdjustableSemaphore
+    ) -> None:
+        """
+        Load a chunk of the file.
+
+        Args:
+            temp_file_path (Path): Path to the temporary file.
+            start (int): Start byte of the chunk.
+            end (int): End byte of the chunk.
+            semaphore (AdjustableSemaphore): Semaphore to control concurrency.
+        """
         await semaphore.acquire()
         try:
             for i in range(self.max_retries):
@@ -161,30 +183,11 @@ class FileDownloader:
                         headers.update(self.custom_headers)
 
                     if self.curl_cffi_required:
-                        try:
-                            response = await self.session.get(
-                                url=self.url, headers=headers
-                            )
-                            chunk = response.content
-                            if len(chunk) != end - start + 1:
-                                print(len(chunk), end - start + 1)
-                                raise Exception("Chunk size mismatch")
-                        finally:
-                            try:
-                                response.close()
-                            except:
-                                pass
+                        response = await self.session.get(url=self.url, headers=headers)
+                        chunk = response.content
                     else:
-                        try:
-                            response = await self.session.get(
-                                url=self.url, headers=headers
-                            )
-                            chunk = await response.content.read()
-                        finally:
-                            try:
-                                response.close()
-                            except:
-                                pass
+                        response = await self.session.get(url=self.url, headers=headers)
+                        chunk = await response.content.read()
 
                     async with aiofiles.open(temp_file_path, "r+b") as file:
                         await file.seek(start)
@@ -199,7 +202,7 @@ class FileDownloader:
                     self.log(
                         f"Retrying chunk {start}-{end} ({i + 1}/{self.max_retries})"
                     )
-
+                    await asyncio.sleep(2**i)  # Exponential backoff
         except asyncio.CancelledError:
             pass
         except Exception as e:
@@ -207,7 +210,14 @@ class FileDownloader:
         finally:
             await semaphore.release()
 
-    async def temp_file_creator(self, temp_file_path, total_chunks) -> tuple:
+    async def temp_file_creator(self, temp_file_path: Path, total_chunks: int) -> None:
+        """
+        Create a temporary file with the specified size.
+
+        Args:
+            temp_file_path (Path): Path to the temporary file.
+            total_chunks (int): Total number of chunks.
+        """
         async with aiofiles.open(temp_file_path, "wb") as file:
             for i in range(total_chunks):
                 start = i * self.chunk_size
@@ -215,7 +225,13 @@ class FileDownloader:
                 await file.write(b"\0" * (end - start + 1))
                 self.size_done += end - start + 1
 
-    async def dynamic_worker_updater(self, semaphore) -> None:
+    async def dynamic_worker_updater(self, semaphore: AdjustableSemaphore) -> None:
+        """
+        Dynamically update the number of workers based on download speed.
+
+        Args:
+            semaphore (AdjustableSemaphore): Semaphore to control concurrency.
+        """
         prev_downloaded = 0
         prev_speed = 0
 
@@ -223,6 +239,7 @@ class FileDownloader:
             if self.size_done >= self.total_size:
                 break
             await asyncio.sleep(self.dynamic_workers_update_interval)
+
             speed = (
                 self.size_done - prev_downloaded
             ) / self.dynamic_workers_update_interval
@@ -241,49 +258,39 @@ class FileDownloader:
         """
         Perform a single-threaded download of the file.
         """
-
         for i in range(self.max_retries):
             try:
                 if self.curl_cffi_required:
-                    try:
-                        response = await self.session.get(
-                            url=self.url, headers=self.custom_headers, stream=True
-                        )
-                        async with aiofiles.open(self.output_path, "wb") as output_file:
-                            async for chunk in response.aiter_content():
-                                await output_file.write(chunk)
-                                self.size_done += len(chunk)
-                    except Exception as e:
-                        raise e
-                    finally:
-                        try:
-                            response.close()
-                        except:
-                            pass
+                    response = await self.session.get(
+                        url=self.url, headers=self.custom_headers, stream=True
+                    )
+                    async with aiofiles.open(self.output_path, "wb") as output_file:
+                        async for chunk in response.aiter_content():
+                            await output_file.write(chunk)
+                            self.size_done += len(chunk)
+                    response.close()
                 else:
-                    try:
-                        response = await self.session.get(
-                            self.url, headers=self.custom_headers
-                        )
-                        async with aiofiles.open(self.output_path, "wb") as output_file:
-                            while chunk := await response.content.read(self.chunk_size):
-                                await output_file.write(chunk)
-                                self.size_done += len(chunk)
-                    finally:
-                        try:
-                            response.close()
-                        except:
-                            pass
+                    response = await self.session.get(
+                        self.url, headers=self.custom_headers
+                    )
+                    async with aiofiles.open(self.output_path, "wb") as output_file:
+                        while chunk := await response.content.read(self.chunk_size):
+                            await output_file.write(chunk)
+                            self.size_done += len(chunk)
+                    response.close()
                 break
             except Exception as e:
                 self.log(f"Error downloading file: {e}")
                 if i == self.max_retries - 1:
                     raise e
                 self.log(f"Retrying download ({i + 1}/{self.max_retries})")
+                await asyncio.sleep(2**i)  # Exponential backoff
 
-    async def multi_threaded_download(self):
+    async def multi_threaded_download(self) -> None:
+        """
+        Perform a multi-threaded download of the file.
+        """
         total_chunks = (self.total_size + self.chunk_size - 1) // self.chunk_size
-
         temp_file_path = self.output_dir / (self.output_path.stem + ".temp")
         self.log(
             f"Creating Temp File {temp_file_path.name} of size {self.total_size} bytes"
@@ -316,7 +323,6 @@ class FileDownloader:
         tasks.append(self.show_progress("Downloading"))
 
         await self.task_runner(tasks)
-
         temp_file_path.rename(self.output_path)
 
     async def start(self) -> Path:
@@ -332,24 +338,15 @@ class FileDownloader:
         for i in range(self.max_retries):
             try:
                 if self.session:
-                    try:
-                        await self.session.close()
-                    except:
-                        pass
+                    await self.session.close()
                 self.session = aiohttp.ClientSession(
                     timeout=aiohttp.ClientTimeout(total=self.timeout)
                 )
 
-                try:
-                    response = await self.session.get(
-                        url=self.url, headers=self.custom_headers
-                    )
-                finally:
-                    try:
-                        response.close()
-                    except:
-                        pass
-
+                response = await self.session.get(
+                    url=self.url, headers=self.custom_headers
+                )
+                response.close()
                 self.total_size = int(response.headers.get("Content-Length", 0))
                 if self.total_size == 0:
                     raise Exception("Content-Length header is missing or invalid")
@@ -361,24 +358,15 @@ class FileDownloader:
             except Exception as e:
                 try:
                     self.log(f"Failed to get file info using aiohttp: {e}")
-                    try:
-                        await self.session.close()
-                    except:
-                        pass
+                    await self.session.close()
 
                     self.session = AsyncSession(timeout=self.timeout)
                     self.curl_cffi_required = True
 
-                    try:
-                        response = await self.session.get(
-                            url=self.url, headers=self.custom_headers, stream=True
-                        )
-                    finally:
-                        try:
-                            response.close()
-                        except:
-                            pass
-
+                    response = await self.session.get(
+                        url=self.url, headers=self.custom_headers, stream=True
+                    )
+                    response.close()
                     self.total_size = int(response.headers.get("Content-Length", 0))
                     if self.total_size == 0:
                         raise Exception("Content-Length header is missing or invalid")
@@ -392,12 +380,10 @@ class FileDownloader:
                 except Exception as e:
                     self.log(f"Error getting file info: {e}")
                     if i == self.max_retries - 1:
-                        try:
-                            await self.session.close()
-                        except:
-                            pass
+                        await self.session.close()
                         raise e
                     self.log(f"Retrying getting file info ({i + 1}/{self.max_retries})")
+                    await asyncio.sleep(2**i)  # Exponential backoff
 
         self.output_path = change_file_path_if_exist(self.output_dir / self.filename)
         self.filename = self.output_path.name
@@ -421,12 +407,8 @@ class FileDownloader:
                 await self.single_threaded_download()
         else:
             self.log("Server supports range requests. Starting Multi-Threaded Download")
-
             await self.multi_threaded_download()
 
         self.log(f"Downloaded {self.filename}")
-        try:
-            await self.session.close()
-        except:
-            pass
+        await self.session.close()
         return self.output_path
